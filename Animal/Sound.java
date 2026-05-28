d06b1bc64f86b81c9f0c63223c9a957d79e2183e
Ehsanwebs <shinycookies37@gmail.com>
2026-05-28 20:38:21 +0800

    Add files via upload

---
+++ Animal/Sound.java
@@ -0,0 +1,5 @@
+package Animal;
+
+public interface Sound {
+    void makeSound();
+}
---
+++ Animal/main.java
@@ -0,0 +1,11 @@
+package Animal;
+import Animal.Mammal.Dog;
+public class main {
+    public static void main(String[] args) {
+        Dog dog1=new Dog("D101","Buds",3,12.5);
+
+        dog1.displayInfo();
+        dog1.makeSound();
+    }
+    
+}
