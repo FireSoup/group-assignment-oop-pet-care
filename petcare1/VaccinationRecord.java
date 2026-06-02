5722d1e7431d3ee16b06510726dbced6a7fed018
Austine M <austineten301@gmail.com>
2026-06-02 13:12:41 +0800

    Update VaccinationRecord.java
    Latest updated vaccination record module

--- petcare1/VaccinationRecord.java
+++ petcare1/VaccinationRecord.java
@@ -1,115 +1,67 @@
-package vaccination;
-
+package petcare1;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-//Action Class
-public class VaccinationRecord {
-    protected Pet patient;
+abstract class InventoryItem {
+    private String itemId, name; private int stockQuantity; private double price; 
+    public InventoryItem(String itemId, String name, int stockQuantity, double price) {
+        this.itemId = itemId; this.name = name; this.stockQuantity = stockQuantity; this.price = price;
+    }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getStockQuantity() { return stockQuantity; }
+    public void setStockQuantity(int stockQuantity) { this.stockQuantity = stockQuantity; }
+}
+
+class Vaccine extends InventoryItem {
+    private String vaccineId, manufacturer, batchNumber; private int validityMonths;
+    public Vaccine(String itemId, String name, int stockQuantity, double price, String vaccineId, String manufacturer, String batchNumber, int validityMonths) {
+        super(itemId, name, stockQuantity, price);
+        this.vaccineId = vaccineId; this.manufacturer = manufacturer; this.batchNumber = batchNumber; this.validityMonths = validityMonths;
+    }
+    public int getValidityMonths() { return validityMonths; }
+}
+
+// NEW: Centralized Database for Vaccines
+class VaccineDirectory {
+    public static List<Vaccine> loadAvailableVaccines() {
+        List<Vaccine> vaccines = new ArrayList<>();
+        vaccines.add(new Vaccine("I-1", "Parvovirus Vaccine", 100, 60.00, "V-1", "Pfizer", "B-099", 12));
+        vaccines.add(new Vaccine("I-2", "Rabies Booster", 150, 45.00, "V-2", "Moderna", "B-101", 12));
+        vaccines.add(new Vaccine("I-3", "Feline Leukemia", 80, 50.00, "V-3", "Zoetis", "B-205", 24));
+        return vaccines;
+    }
+}
+
+public class VaccinationRecord implements Billable {
+    protected Animals patient; 
     protected Vaccine administredVaccine;
     protected Veterinarian attedingVet;
+    private LocalDate administrationDate, nextDueDate;
 
-    private LocalDate administrationDate;
-    private LocalDate nextDueDate;
-
-    public VaccinationRecord(Pet patient, Vaccine administredVaccine, Veterinarian attedingVet, LocalDate administrationDate) {
-        this.patient = patient;
-        this.administredVaccine = administredVaccine;
-        this.attedingVet = attedingVet;
-        this.administrationDate = administrationDate;
-
-        this.nextDueDate = calculateNextDueDate(administrationDate, administredVaccine.getValidityMonths());
-
-        // Simple stock reduction using basic getter and setter
-        int currentStock = this.administredVaccine.getStockQuantity();
-        this.administredVaccine.setStockQuantity(currentStock - 1);
+    public VaccinationRecord(Animals patient, Vaccine administredVaccine, Veterinarian attedingVet, LocalDate administrationDate) {
+        this.patient = patient; this.administredVaccine = administredVaccine;
+        this.attedingVet = attedingVet; this.administrationDate = administrationDate;
+        this.nextDueDate = administrationDate.plusMonths(administredVaccine.getValidityMonths());
+        administredVaccine.setStockQuantity(administredVaccine.getStockQuantity() - 1);
     }
 
-    private LocalDate calculateNextDueDate(LocalDate dataGiven, int monthsValid) {
-        return dataGiven.plusMonths(monthsValid);
+    // CHANGED: Now returns a String instead of using System.out.println
+    public String getRecordDetails() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n===============================\n");
+        sb.append("    OFFICIAL VACCINATION RECORD  \n");
+        sb.append("=================================\n");
+        sb.append("Patient Name: ").append(patient.petName).append("\n");
+        sb.append("Doctor      : ").append(attedingVet.getName()).append("\n");
+        sb.append("Vaccine     : ").append(administredVaccine.getName()).append("\n");
+        sb.append("NEXT DUE    : ").append(nextDueDate).append("\n");
+        sb.append("================================\n");
+        return sb.toString();
     }
 
-    public Pet getPatient() { return patient; }
-    public Vaccine getVaccine() { return administredVaccine; }
-    public Veterinarian getAttedingVet() { return attedingVet; }
-    public LocalDate getAdministrationDate() { return administrationDate; }
-    public LocalDate getNextDueDate() { return nextDueDate; }
-
-    public void printRecordDetails() {
-        System.out.println("\n===============================");
-        System.out.println("    OFFICIAL VACCINATION RECORD  ");
-        System.out.println("=================================");
-        System.out.println("Patient Name: " +  patient.getName());
-        System.out.println("Doctor      : " + attedingVet.getName());
-        System.out.println("__________________________________");
-        System.out.println("Vaccine     : " + administredVaccine.getName());
-        System.out.println("Batch No.   : " + administredVaccine.getBatchNumber());
-        System.out.println("Date Given  : " + administrationDate);
-        System.out.println("NEXT DUE    : " + nextDueDate);
-        System.out.println("================================\n");
-    }
-}
-
-//Child Class
-class Vaccine extends InventoryItem {
-    private String vaccineId;
-    private String manufacturer;
-    private String batchNumber;
-    private int validityMonths;
-
-    public Vaccine(String itemId, String name, int stockQuantity, String vaccineId, String manufacturer, String batchNumber, int validityMonths) {
-        super(itemId, name, stockQuantity);
-        this.vaccineId = vaccineId;
-        this.manufacturer = manufacturer;
-        this.batchNumber = batchNumber;
-        this.validityMonths = validityMonths;
-    }
-
-    public String getVaccineId() { return vaccineId; }
-    public String getManufacturer() { return manufacturer; }
-    public String getBatchNumber() { return batchNumber; }
-    public int getValidityMonths() { return validityMonths; }
-
-    public void setVaccineId(String vaccineId) { this.vaccineId = vaccineId; }
-    public void setManufacturer(String manufacturer) { this.manufacturer = manufacturer; }
-    public void setBatchNumber(String batchNumber) { this.batchNumber = batchNumber; }
-    public void setValidityMonths(int validityMonths) { this.validityMonths = validityMonths; }
-
-    @Override
-    public void displayDetails() {
-        System.out.println("[Vaccine] " + getName() + " | Batch: " + batchNumber);
-    }
-}
-
-//Parent Class
-abstract class InventoryItem {
-    private String itemId;
-    private String name;
-    private int stockQuantity;
-
-    public InventoryItem(String itemId, String name, int stockQuantity) {
-        this.itemId = itemId;
-        this.name = name;
-        this.stockQuantity = stockQuantity;
-    }
-
-    public String getItemId() { return itemId; }
-    public String getName() { return name; }
-    public int getStockQuantity() { return stockQuantity; }
-
-    public void setStockQuantity(int stockQuantity) {
-        this.stockQuantity = stockQuantity;
-    }
-    public abstract void displayDetails();
-}
-
-//My Dummy files
-class Pet {
-    private String name;
-    public Pet(String name) { this.name = name; }
-    public String getName() { return name; }
-}
-class Veterinarian {
-    private String name;
-    public Veterinarian(String name) { this.name = name; }
-    public String getName() { return name; }
+    @Override public String getItemName() { return administredVaccine.getName() + " Vaccine"; }
+    @Override public double getPrice() { return administredVaccine.getPrice(); }
+    @Override public String getAssignedDoctor() { return attedingVet.getName(); }
 }
